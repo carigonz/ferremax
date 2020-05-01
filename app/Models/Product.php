e86d2fadb29dbf310f6c0ec68612c3302bc71775
Carolina Gonzalez <carolinagonzalez794@gmail.com>
2020-04-30 22:25:48 -0300

    added debugbar to project

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Class product 
  * 
- * @property Tariff $tariff_id
  * @property string $code
  * @property string $name
  * @property Tariff $tariff_id
